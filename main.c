9a686ad53e729c581c454f7030792b7e3dc16510
Issaimaru <80198387+Issaimaru@users.noreply.github.com>
2025-05-08 12:01:58 +0900

    :sparkles: 棒制御，球とブロックのAABB衝突判定の追加

--- main.c
+++ main.c
@@ -1,6 +1,8 @@
 //
 // Created by Asahi Isshiki on 25/05/08.
 int main(void) {
+    printf("Hello, World!");
+    printf("\rHey!\n");
     return 0;
 }
 //
---
+++ structs.h
@@ -0,0 +1,29 @@
+#ifndef STRUCTS_H
+#define STRUCTS_H
+
+struct Bar {
+    int x;          // バーのX座標
+    int y;          // バーのY座標
+    int width;      // バーの幅
+    int height;     // バーの高さ
+    int speed;      // バーの移動速度
+};
+
+struct Ball {
+    int x;          // ボールのX座標
+    int y;          // ボールのY座標
+    int radius;     // ボールの半径
+    int dx;         // ボールのX方向の速度
+    int dy;         // ボールのY方向の速度
+};
+
+struct Block {
+    int x;          // ブロックのX座標
+    int y;          // ブロックのY座標
+    int width;      // ブロックの幅
+    int height;     // ブロックの高さ
+    bool isDestroyed; // ブロックが壊れているかどうか
+};
+
+
+#endif // STRUCTS_H
