297e369c4c1d66a8685b6b617c64bb940f85bc57
Zypr <44808225+ZyprA@users.noreply.github.com>
2025-05-22 13:37:29 +0900

    修正する箇所

--- main.c
+++ main.c
@@ -1,13 +1,10 @@
 #include <stdio.h>
-#include "unit.h"
 #include "define.h"
+#include "structs.h"
 #include "io.h"
 
 
 char buffer[SCREEN_HEIGHT][SCREEN_WIDTH];
-unit* units[UNIT_MAX];
-int unitCount = 0;
-unit* player;
 int end = 0;
 
 void init_buffer() {
@@ -29,44 +26,15 @@ void print_buffer() {
 }
 #endif
 
-void deploy_unit(unit* target) {
-    if (!target || !target->shape) return;
-    for (int dy = 0; dy < target->height; dy++) {
-        for (int dx = 0; dx < target->width; dx++) {
-            const int sx = target->x + dx;
-            const int sy = target->y + dy;
 
-            if (sx >= 0 && sx < SCREEN_WIDTH && sy >= 0 && sy < SCREEN_HEIGHT) {
-                buffer[sy][sx] = target->shape[dy][dx];
-            }
-        }
-    }
-}
+// deploy block, deploy wall, deploy bar
 
-void add_unit(unit* unit) {
-    if (unitCount < UNIT_MAX) {
-        units[unitCount++] = unit;
-    }
-}
+//
 
-void remove_unit(unit* unit) {
-    for (int i = 0; i < unitCount; i++) {
-        if (units[i] == unit) {
-            for (int k = i; k < unitCount - 1; k++) {
-                units[k] = units[k+1];
-            }
-            unitCount--;
-            break;
-        }
-    }
-}
 
 void update_buffer() {
     init_buffer();
-     for (int i = 0; i < unitCount; i++) {
-        unit* unit = units[i];
-        deploy_unit(unit);
-     }
+    // deploy wall, ball, bar
 }
 
 #if !defined(NATIVE_MODE)
@@ -85,26 +53,7 @@ int main() {
     init_buffer();
     print_buffer();
 
-    const char* shape1[] = {
-        "-----"
-    };
-
-    const char* wall[] = {
-        "|", "|", "|", "|", "|", "|", "|", "|", "|", "|", "|", "|", "|", "|", "|", "|", "|", "|", "|", "|", "|", "|", "|", "|", "|"
-    };
-
-    const char* cell[] = {
-        "================================================================================"
-    };
-
-    player = create_unit(3,3,5,1,shape1);
-    unit* wall_left = create_unit(0, 0, 1, SCREEN_HEIGHT, wall);
-    unit* wall_right = create_unit(SCREEN_WIDTH - 1, 0, 1, SCREEN_HEIGHT, wall);
-
-
-    add_unit(player);
-    add_unit(wall_left);
-    add_unit(wall_right);
+    // ここにball, wall, bar
 
     set_mtimer_interval(300);
     timer_interrupt_hook = timer_hook;
@@ -122,15 +71,13 @@ int main() {
             sysctrl->mtimecmp = (unsigned long long)-1;
         } else if (ch == 'a' || ch == 'd'){
             if (ch == 'a') {
-                player->x--;
+                // movebar
             } else {
-                player->x++;
+                // movebar
             }
             update_buffer();
         }
      }
-
-    destroy_unit(player);
 
     return 0;
 #endif
